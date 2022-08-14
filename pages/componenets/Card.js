d6a9da78e7d04b6399b98ce23513bfff6d853067
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-08-14 14:38:26 -0300

    init

--- pages/componenets/Card.js
+++ pages/componenets/Card.js
@@ -3,21 +3,17 @@ import { useCallback } from 'react';
 import styles from '../../styles/card.module.css'
 
 
-export class Card extends React.Component {
+export function Card(props) {
 
-    componentDidMount() {
-        console.log("yohalo")
-    }
+    const data = props.data;
 
-    render() {
-        var data = this.props.data;
-        return (
-            <div className={styles.card_container}  >
-                <img src={data ? data.img : "alat"} alt="" />
-                <p>{data ? data.name : "alt"}</p>
-            </div>
-        )
-    }
+    return (
+        <div className={styles.card_container}  >
+            <img src={data ? data.img : "alat"} alt="" />
+            <p>{data ? data.name : "alt"}</p>
+        </div>
+    )
+
 }
 
 export default Card;
--- pages/componenets/Cardinfos.js
+++ pages/componenets/Cardinfos.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 
 export function Cardinfos(props) {
-
-    //const [data, setdata] = useState(props.infos)
-
+    const data = props.infos;
+    console.log(data)
     return (
-        <h1>rolfoldo</h1>
+        <h1>{data ? data.name : "alt"}</h1>
     )
 }
 export default Cardinfos;
