4fd4731e589d5c0758b1f394ae51aa2726510501
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-08-14 20:35:04 -0300

    init

---
+++ pages/componenets/Aside.js
@@ -0,0 +1,35 @@
+import styles from "../../styles/aside.module.css"
+import Link from 'next/link'
+import { GrFormSearch } from 'react-icons/gr';
+
+export function Aside() {
+
+
+    return (
+        <aside className={styles.aside}>
+            <div className={styles.logo}> <img src="https://www.rederpg.com.br/wp/wp-content/uploads/2020/05/DD-Logo.png" alt="" /></div>
+            <fieldset>
+                <input type="text" placeholder='Search Something' />
+                <GrFormSearch className={styles.icon} />
+            </fieldset>
+            <nav>
+                <Link {...{
+                    href: "../",
+                    query: {
+                        message: "message"
+                    }
+                }} as="/" >
+                    <a >CHARACTERS</a>
+                </Link>
+                <Link href="./componenets/Maps">
+                    <a>MAPS</a>
+                </Link>
+                <a href="#">GUILDS</a>
+                <a href="#">HISTORIES</a>
+                <a href="#">DICES</a>
+            </nav>
+        </aside>
+    )
+}
+
+export default Aside;
---
+++ pages/componenets/Maps.js
@@ -0,0 +1,7 @@
+export function Maps() {
+    return (
+        <h1>RELOU MOTO </h1>
+    )
+}
+
+export default Maps;
