c646adeaa45df56910c84ceb9963a85caf29bc5a
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-08-15 18:00:15 -0300

    init

--- pages/componenets/Cardinfos.js
+++ pages/componenets/Cardinfos.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 
 export function Cardinfos(props) {
-
     const data = props.infos;
 
+    function pix() {
+        props.removetab(data.id)
+    }
+
     return (
-        <div>
+        <div >
             <p onClick={() => { props.func({ name: data.name, img: data.img }) }}>{data ? data.name : "alt"}</p>
+            <button onClick={pix}>Del</button>
         </div>
     )
 }
